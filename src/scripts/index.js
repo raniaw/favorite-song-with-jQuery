c37537f7f7d6d0e6dcf180c93e7d45284ceb3aa8
raniaw <raniaw@web.de>
2019-12-16 23:24:50 +0100

    uninstall gh-pages

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,16 +12,21 @@ import imgFluegel from '../images/fluegel_260.png';
 
 // mp3 einbinden
 var src = require('../media/Chris-De-Burgh-The-Lady-In-Red.mp3');
-
+var ladyImg = require('../images/ladyInRed-530-800.png');
+var chrisImg = require('../images/chris.png');
+var fluegelImg = require('../images/fluegel_260.png');
 //document.getElementById('audio').src = src;
+console.log(ladyImg.default)
+jQuery(document).ready(function (f) {
 
-jQuery(document).ready(function(f) {
-  
     $("#audio").src = src;
 
-   //image find and set attrribute
-    $(this).find('#lady').attr("src", imgLady);
-    $(this).find('#chris').attr("src",imgChris);
-    $(this).find('#fluegel').attr("src",imgFluegel);
+    //image find and set attrribute
+    $(this).find('#lady').attr("src", ladyImg.default);
+    //  $(this).find('#lady').attr("src", imgLady);
+    $(this).find('#chris').attr("src", chrisImg.default);
+    //$(this).find('#chris').attr("src", imgChris);
+    $(this).find('#fluegel').attr("src", fluegelImg.default);
+    // $(this).find('#fluegel').attr("src", imgFluegel);
     console.log(ladySrc.default)
 });
